22868308277a165d3ac0c8bd9d2d5e3d3b2a25f2
Akshay Pratap Singh <pratapakshay0@gmail.com>
2016-07-01 17:28:59 +0530

    login added

--- app/index.php
+++ app/index.php
@@ -1,3 +1,26 @@
+<?php
+$mac=$_POST['mac'];
+$ip=$_POST['ip'];
+$username=$_POST['username'];
+$linklogin=$_POST['link-login'];
+$linkorig=$_POST['link-orig'];
+$error=$_POST['error'];
+$trial=$_POST['trial'];
+$loginby=$_POST['login-by'];
+$chapid=$_POST['chap-id'];
+$chapchallenge=$_POST['chap-challenge'];
+$linkloginonly=$_POST['link-login-only'];
+$linkorigesc=$_POST['link-orig-esc'];
+$macesc=$_POST['mac-esc'];
+$identity=$_POST['identity'];
+$bytesinnice=$_POST['bytes-in-nice'];
+$bytesoutnice=$_POST['bytes-out-nice'];
+$sessiontimeleft=$_POST['session-time-left'];
+$uptime=$_POST['uptime'];
+$refreshtimeout=$_POST['refresh-timeout'];
+$linkstatus=$_POST['link-status'];
+?>
+
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
@@ -33,7 +56,7 @@
   <div class="container">
     <div class="row ">
       <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 login">
-        <form id="userForm" role="form" data-toggle="validator" method="POST">
+        <form id="userForm" action="<?php echo $linkloginonly; ?>" role="form" data-toggle="validator" method="POST">
           <fieldset>
 
             <legend class="legend">91springboard</legend>
@@ -53,6 +76,8 @@
             </div>
 
             <div class="input" id="input-hidden-fields">
+              <input type="hidden" name="dst" value="<?php echo $linkorig; ?>"/>
+              <input type="hidden" name="popup" value="true"/>
             </div>
 
             <div class="input form-group col-xs-12">
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -54,85 +54,6 @@ function get_query_params() {
   return queryParams;
 }
 
-//get the IP addresses associated with an account
-function getInternalIP(callback){
-  var ip_dups = {};
-
-  //compatibility for firefox and chrome
-  var RTCPeerConnection = window.RTCPeerConnection
-    || window.mozRTCPeerConnection
-    || window.webkitRTCPeerConnection;
-  var useWebKit = !!window.webkitRTCPeerConnection;
-
-  //bypass naive webrtc blocking using an iframe
-  if(!RTCPeerConnection){
-    //NOTE: you need to have an iframe in the page right above the script tag
-    //
-    //<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>
-    //<script>...getIPs called in here...
-    //
-    var win = iframe.contentWindow;
-    RTCPeerConnection = win.RTCPeerConnection
-      || win.mozRTCPeerConnection
-      || win.webkitRTCPeerConnection;
-    useWebKit = !!win.webkitRTCPeerConnection;
-  }
-
-  //minimal requirements for data connection
-  var mediaConstraints = {
-    optional: [{RtpDataChannels: true}]
-  };
-
-  var servers = {iceServers: [{urls: "stun:stun.services.mozilla.com"}]};
-
-  //construct a new RTCPeerConnection
-  var pc = new RTCPeerConnection(servers, mediaConstraints);
-
-  function handleCandidate(candidate){
-    //match just the IP address
-    var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-    var ip_addr = ip_regex.exec(candidate)[1];
-
-    //remove duplicates
-    if(ip_dups[ip_addr] === undefined && Object.keys(ip_dups).length ==0)
-      callback(ip_addr);
-
-    ip_dups[ip_addr] = true;
-  }
-
-  //listen for candidate events
-  pc.onicecandidate = function(ice){
-
-    //skip non-candidate events
-    if(ice.candidate)
-      handleCandidate(ice.candidate.candidate);
-  };
-
-  //create a bogus data channel
-  pc.createDataChannel("");
-
-  //create an offer sdp
-  pc.createOffer(function(result){
-
-    //trigger the stun server request
-    pc.setLocalDescription(result, function(){}, function(){});
-
-  }, function(){});
-
-  //wait for a while to let everything done
-  setTimeout(function(){
-    //read candidate info from local description
-    var lines = pc.localDescription.sdp.split('\n');
-
-    lines.forEach(function(line){
-      if(line.indexOf('a=candidate:') === 0) {
-        handleCandidate(line);
-      }
-    });
-  }, 1000);
-}
-
-
 $(function() {
 
   console.log('Welcome to 91springboard Captive Portal Login Console');
@@ -151,31 +72,6 @@ $(function() {
       formInValidActions();
     }
   });
-
-  var queryParams = get_query_params();
-
-  if(queryParams.res == "failed"){
-    formSubmitErrorActions();
-
-    delete queryParams.res;
-    delete queryParams.auth;
-  }
-
-  var params_required = ["url", "proxy", "uip", "client_mac"];
-
-  Object.keys(queryParams).forEach(function (param) {
-
-    if (params_required.indexOf(param) > -1 ){
-      console.log("Param '" + param + "' = " + queryParams[param]);
-      $("<input type='hidden' value='' />").attr("name", param).attr("value", queryParams[param]).appendTo("#input-hidden-fields");
-    }
-  });
-
-  $("#userForm").attr("action", "https://vsz.91springboard.com/submitForm");
-
-  getInternalIP(function (ip) {
-    console.log("Your internal IP ::  " + ip);
-  });
-
+  
 
 }());
